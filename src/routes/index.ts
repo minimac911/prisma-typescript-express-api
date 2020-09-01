9fba4d8e5856204bacd31daad9b2f9ecbeb9c824
Thomas McAlpine <thomasmcalpine77@gmail.com>
2020-09-01 15:56:58 +0200

    🍕 Added DB connection .env

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const router: Router = Router();
 
-router.use("/tags");
+router.use("/user", function (req, res) {
+    res.send('hello world')
+});
 
 export const MainRouter: Router = router;
