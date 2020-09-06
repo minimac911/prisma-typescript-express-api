005231b883e1b9ab04dea5c7fa8f4cb4c50e1e69
Thomas McAlpine <thomasmcalpine77@gmail.com>
2020-09-06 16:26:24 +0200

    Removed index.ts from routes

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router: Router = Router();
-
-router.use("/user", function (req, res) {
-    res.send('hello world')
-});
-
-export const MainRouter: Router = router;
